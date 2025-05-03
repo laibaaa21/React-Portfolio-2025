a327e31481732bcc9087c51ad2baf3ac3cb70f1d
laibaaa21 <laibasaqlain92@gmail.com>
2025-05-03 18:39:38 +0500

    Fixed compatibility issues

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+
 import AppRouter from './router';
 
 const App = () => {
   return (
-    <div className="App">
-      <AppRouter />
-    </div>
+      <div className="App">
+        <AppRouter />
+      </div>
   );
 }
 
